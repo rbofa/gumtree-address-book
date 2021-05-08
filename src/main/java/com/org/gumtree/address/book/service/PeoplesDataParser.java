d212825f38da093fc6848c95c0b5aca7a73296b1
Ramesh <ramesh.bofa@gmail.com>
2021-05-08 09:37:44 +0100

    RP: Address book File reading and people data loading

--- src/main/java/com/org/gumtree/address/book/service/PeoplesDataParser.java
+++ src/main/java/com/org/gumtree/address/book/service/PeoplesDataParser.java
@@ -2,12 +2,35 @@ package com.org.gumtree.address.book.service;
 
 import com.org.gumtree.address.book.domain.Person;
 
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PeoplesDataParser {
 
+    private static String fileName = "input/AddressBook";
+
     public List<Person> getPeople() {
+        return loadAndReadAddressBookFile();
+    }
+
+    private List<Person> loadAndReadAddressBookFile() {
+        final URL url = getClass().getClassLoader().getResource(fileName);
+        try (Stream<String> lines = Files.lines(Paths.get(url.getPath()))) {
+            return lines.map(line -> {
+                final String[] lineSplit = line.split(",");
+                return new Person(lineSplit[0].trim(), lineSplit[1].trim(), lineSplit[2].trim());
+            }).collect(Collectors.toList());
+
+        } catch (Exception ex) {
+            //TODO: log : "Unable to read address book input file"
+        }
+
         return new ArrayList<>();
     }
+
 }
--- src/test/java/com/org/gumtree/address/book/domain/PersonTest.java
+++ src/test/java/com/org/gumtree/address/book/domain/PersonTest.java
@@ -1,12 +1,15 @@
 package com.org.gumtree.address.book.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("Person Domain Scenario")
 public class PersonTest {
 
     @Test
+    @DisplayName("Person data serialization or binding")
     public void shouldParseFilePersonTest() {
         Person person = new Person("Bill McKnight", "Male", "16/03/77");
         assertThat(person.getName()).isEqualTo("Bill McKnight");
